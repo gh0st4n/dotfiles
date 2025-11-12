8beb36577c097bb37065a9e379a385746aacdb33
Mr-4lone <darskevoid@gmail.com>
2025-11-12 22:32:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-echo "Hello World"
+#!/bin/bash
+
+# Installation
+#-- BSPWM
+bspwm():
+sudo xbps-install -Su bspwm sxhkd picom polybar rofi dunst yay thunar xterm alacritty xfce4-terminal xorg-minimal
+
+#-- i3 WM
+i3():
+sudo xbps-install -Su i3-gaps picom polybar rofi dunst yay thunar xterm alacritty xfce4-terminal xorg-minimal
